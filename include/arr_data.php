ea37b8bee099025c63d0af13bc7862a031c1b332
liondot <51257576+liondot@users.noreply.github.com>
2022-11-01 15:25:48 +0900

    yoogane landing

---
+++ include/arr_data.php
@@ -0,0 +1,154 @@
+<?
+
+$site_title = "유가네닭갈비"; // 사이트명
+
+$P_lang = "ko"; // html language
+
+$P_company_name = ""; // 회사명
+
+$P_addr1 = ""; // 주소1
+$P_addr2 = ""; // 주소2
+
+$P_tel1 = "";
+$P_tel2 = "";
+
+$P_fax1 = "";
+$P_fax2 = "";
+
+$P_email1 = "";
+$P_email2 = "";
+
+$P_business_no = ""; // 사업자등록번호
+
+$P_sales_no = ""; // 통신판매업신고번호
+
+// 네비게이션 메뉴명
+$nav_1 = "회사소개";
+$nav_1_1 = "회사개요";
+$nav_1_2 = "대표인사말";
+$nav_1_3 = "미션 & 비전";
+$nav_1_4 = "연혁";
+$nav_1_5 = "오시는길";
+
+$nav_2 = "사업영역";
+$nav_2_1 = "사업개요";
+$nav_2_2 = "글로벌 토이";
+$nav_2_2_1 = "디자인";
+$nav_2_2_2 = "글로벌 네트워크";
+$nav_2_2_3 = "품질경영";
+$nav_2_3 = "국내 토이";
+$nav_2_3_1 = "상품기획";
+$nav_2_3_2 = "토이유통";
+$nav_2_3_3 = "토이플러스";
+$nav_2_3_4 = "ODM";
+$nav_2_4 = "콘텐츠";
+$nav_2_4_1 = "제작";
+$nav_2_4_2 = "배급";
+$nav_2_4_3 = "라이선스";
+$nav_2_4_4 = "프로모션";
+$nav_2_5 = "바이오";
+
+$nav_3 = "브랜드";
+$nav_3_1 = "글로벌 토이";
+$nav_3_2 = "국내 토이";
+$nav_3_3 = "콘텐츠";
+
+$nav_4 = "투자정보";
+$nav_4_1 = "재무정보";
+$nav_4_1_1 = "요약 연결재무상태표";
+$nav_4_1_2 = "요약 연결손익계산서";
+$nav_4_2 = "주가정보";
+$nav_4_3 = "공시정보";
+$nav_4_3_1 = "전자공시";
+$nav_4_3_2 = "자료실";
+$nav_4_4 = "News";
+
+$nav_5 = "SHOP";
+$nav_5_1 = "SHOP";
+
+$nav_6 = "인재채용";
+$nav_6_1 = "인재상";
+$nav_6_2 = "복리후생";
+
+$nav_7 = "고객센터";
+$nav_7_1 = "A/S 절차 안내";
+$nav_7_2 = "서비스 규정 상세";
+$nav_7_3 = "파트너십(제휴) 안내";
+
+$nav_8 = "매장안내";
+$nav_8_1 = "매장안내";
+
+$nav_policy = "홈페이지 정책";
+$nav_policy_1 = "개인정보처리방침";
+$nav_policy_2 = "이용약관";
+$nav_policy_3 = "이메일무단수집거부";
+
+// 네비게이션 하이퍼링크
+$nav_1_href = "../about/overview.html";
+$nav_1_1_href = "../about/overview.html";
+$nav_1_2_href = "../about/greeting.html";
+$nav_1_3_href = "../about/mission.html";
+$nav_1_4_href = "../about/history.html";
+$nav_1_5_href = "../about/location.html";
+
+$nav_2_href = "../business/overview.html";
+$nav_2_1_href = "../business/overview.html";
+$nav_2_2_href = "../business/global__design.html";
+$nav_2_2_1_href = "../business/global__design.html";
+$nav_2_2_2_href = "../business/global__network.html";
+$nav_2_2_3_href = "../business/global__quality_management.html";
+$nav_2_3_href = "../business/domestic__product_planning.html";
+$nav_2_3_1_href = "../business/domestic__product_planning.html";
+$nav_2_3_2_href = "../business/domestic__toy_distribution.html";
+$nav_2_3_3_href = "../business/domestic__toy_plus.html";
+$nav_2_3_4_href = "../business/domestic__odm.html";
+$nav_2_4_href = "../business/content__produce.html";
+$nav_2_4_1_href = "../business/content__produce.html";
+$nav_2_4_2_href = "../business/content__distribute.html";
+$nav_2_4_3_href = "../business/content__license.html";
+$nav_2_4_4_href = "../business/content__promotion.html";
+$nav_2_5_href = "../business/bio.html";
+
+$nav_3_href = "../brand/global.html";
+$nav_3_1_href = "../brand/global.html";
+$nav_3_2_href = "../brand/domestic.html";
+$nav_3_3_href = "../brand/content.html";
+
+$nav_4_href = "../investment/summary__finance.html";
+$nav_4_1_href = "../investment/summary__finance.html";
+$nav_4_1_1_href = "../investment/summary__finance.html";
+$nav_4_1_2_href = "../investment/summary__profit_or_loss.html";
+$nav_4_2_href = "../investment/stockInformation.html";
+$nav_4_3_href = "../investment/ir__disclosureInformation.html";
+$nav_4_3_1_href = "../investment/ir__disclosureInformation.html";
+$nav_4_3_2_href = "../investment/ir__data.html";
+$nav_4_4_href = "../investment/news.html";
+
+$nav_5_href = "https://www.toyplus.co.kr/";
+$nav_5_1_href = "https://www.toyplus.co.kr/";
+
+$nav_6_href = "../recruitment/talent.html";
+$nav_6_1_href = "../recruitment/talent.html";
+$nav_6_2_href = "../recruitment/welfare.html";
+
+$nav_7_href = "../contact_us/as.html";
+$nav_7_1_href = "../contact_us/as.html";
+$nav_7_2_href = "../contact_us/service_regulations.html";
+$nav_7_3_href = "../contact_us/partnership.html";
+
+$nav_8_href = "../store/store.html";
+$nav_8_1_href = "../store/store.html";
+
+$nav_policy_href = "../policy/privacy.html";
+$nav_policy_1_href = "../policy/privacy.html";
+$nav_policy_2_href = "../policy/term.html";
+$nav_policy_3_href = "../policy/email-security.html";
+
+// 서브비주얼
+$sv_1_name = "";
+$sv_2_name = "";
+$sv_3_name = "";
+$sv_4_name = "";
+$sv_5_name = "";
+
+?>
